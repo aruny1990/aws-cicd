c29970a30b927e157e820122f3511b689fb6b801
aruny1990 <aruny19901178@gmail.com>
2025-06-27 19:12:26 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+#echo
+docker pull arunyadav12/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+#echo
+docker run -d -p 5000:5000 arunyadav12/simple-python-flask-app
