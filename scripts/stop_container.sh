cb7b5085dd76de47a882153d46fa68ae83cb3df5
unknown <33333+aruny1990@users.noreply.github.com>
2025-06-28 12:11:53 +0530

    adding the scripts

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,11 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+containerid=$(docker ps | awk 'NR==2 {print $1}')
+
+if [ -n "$containerid" ]; then
+  echo "Stopping container: $containerid"
+  docker rm -f $containerid
+else
+  echo "No running container found."
+fi
