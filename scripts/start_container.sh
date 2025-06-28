d943759fe49c8b49f8989194cb0662dc01c629d1
aruny1990 <aruny19901178@gmail.com>
2025-06-28 12:16:19 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-#echo
+# Stop and remove any container using port 5000
+existing_container=$(docker ps -q --filter "publish=5000")
+
+if [ -n "$existing_container" ]; then
+  echo "Stopping container using port 5000: $existing_container"
+  docker rm -f $existing_container
+fi
+
 docker pull arunyadav12/simple-python-flask-app
 
-# Run the Docker image as a container
-#echo
-docker run -d -p 5000:5000 arunyadav12/simple-python-flask-app
+# Start the new container
+docker run -d --name flask-app -p 5000:5000 arunyadav12/simple-python-flask-app
